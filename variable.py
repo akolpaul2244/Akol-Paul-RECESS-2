a381fd1c1e45aad2ac6a377cfe0acfd55244d6ef
akolpaul2244 <127102142+akolpaul2244@users.noreply.github.com>
2024-06-05 17:52:14 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,21 @@
+
+
+
+name = "Akol Paul"
+RegNo = "22/U/22453"
+StdNo = 2200722453
+campus = "MUK"
+college = "CoCIS"
+course = "BSSE"
+year = 2
+favorite_color = "Blue"
+
+print(".................Student's info...............")
+print("Name:", name)
+print("RegNo:", RegNo)
+print("StdNo:", StdNo)
+print("campus", campus)
+print("College:", college)
+print("Course:", course)
+print("Year:", year)
+print("My favorite color is", favorite_color)
