892b633580343f5418fe0fbafc5c14b694826644
akolpaul2244 <127102142+akolpaul2244@users.noreply.github.com>
2024-06-17 13:28:08 +0300

    Add files via upload

---
+++ function.py
@@ -0,0 +1,23 @@
+#exercise 1
+"""def get_name_age():
+    name = "paul"
+    age = 23
+    return name, age
+name,age = get_name_age()
+print("My name is: " + name,age)"""
+
+coordinates = [(4,2),(8,3),(3,4), (5,0)]
+
+coordinates.sort(key=lambda coordinate:coordinate)
+print(coordinates)
+
+#you tube link https://www.youtube.com/@Jeff_Geoff_Masterclass
+#RYEKO AI
+#ryeko.org
+#project
+#ryekoai.com
+#aisiom.com
+
+
+
+    
